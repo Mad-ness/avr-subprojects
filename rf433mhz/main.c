4762b3fb5b1d97724a8477cfcb6ccde101c26861
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2018-01-26 21:08:06 +0500

    Intermediate save

--- rf433mhz/main.c
+++ rf433mhz/main.c
@@ -1,12 +1,10 @@
-//#define F_CPU 1200000UL
-#include <stdint.h>
 #include <avr/io.h>
-#include <inttypes.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
-
-// https://github.com/rustam-iskenderov/ATtiny13-transmitter-433Mghz
-
+#include <avr/pgmspace.h>
+#include <avr/power.h>
+#include "tm1637.h" // Pins CLK and DIO are predefined, see the tm1637.h file
+#include "rctswitch.h"
+#include "common.h"
 
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
@@ -15,25 +13,60 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-#define PIN_LED PB3
+
+#define PIN_LED PB4
+#define PIN_TRANSMITTER PB4
+
+static uint8_t i = 0;
+
+ISR(PCINT0_vect) {
+  RCTSwitch_interruptHandler();
+}
+
+void display_test(const uint8_t number) {
+    int d1 = number / 100;
+    int d2 = (number - d1*100)/10;
+    int d3 = (number - d1*100 - d2*10);
+    TM1637_display_digit(TM1637_SET_ADR_00H, d1);
+    TM1637_display_digit(TM1637_SET_ADR_01H, d2);
+    TM1637_display_digit(TM1637_SET_ADR_02H, d3);
+
+    //TM1637_display_colon(true);
+    //_delay_ms(200);
+    //TM1637_display_colon(false);
+}
+
 
 void setup(void) {
-    PORTB = (1<<PIN_LED);
-    DDRB |= (1<<PIN_LED);
+    if ( F_CPU == 16000000 ) clock_prescale_set(clock_div_1);
+    attiny_init();
+    RCTSwitch_setup();
+    TM1637_init();
+    DDRB |= (1 << PIN_LED);
 }
 
+uint8_t counter = 250;
 void loop(void) {
-    sbi(PORTB, PIN_LED);
-    _delay_ms(5000);
-    cbi(PORTB, PIN_LED);
-    _delay_ms(5000);
+/*
+    if ( RCTSwitch_available() ) {
+        uint8_t value = RCTSwitch_getValue();
+        RCTSwitch_reset();
+        display_test(value);
+    }
+*/    
+    if (millis() % 1000 == 0) { 
+        display_test(counter);        
+	sbi(PORTB, PIN_LED);
+	_delay_ms(100);
+	cbi(PORTB, PIN_LED);
+	counter -= 10;
+    }
 }
 
-void main(void)
-{
+void main(void) {
     setup();
     for (;;) {
         loop();
-    }; 
+    }
 }
 

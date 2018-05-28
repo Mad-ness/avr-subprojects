eb549c9b8bd6efd79bc956c96b9eff870a4a8ffa
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2018-05-28 23:13:06 +0500

    Updating sources

--- GreenHousev2/src/arm/main.cpp
+++ GreenHousev2/src/arm/main.cpp
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <ghair.h>
 
+GHAir air(123, 120, (byte*)"1Node", (byte*)"2Node");
+
+void AirHandler(AirPacket *pkt) {
+
+}
 
 int main() {
 
+    air.setup();
+    air.setHandler(&AirHandler);
+    air.rf24()->printDetails();
     return 0;
 }
 

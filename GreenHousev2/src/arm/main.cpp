e8cb30aef90b2e0c5fdd1ac7ebef721df481d678
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2018-06-01 20:02:35 +0300

    Updated sources

--- GreenHousev2/src/arm/main.cpp
+++ GreenHousev2/src/arm/main.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <ghair.h>
 
-GHAir air(123, 120, (byte*)"1Node", (byte*)"2Node");
+GHAir air(42, 43, (byte*)"1Node", (byte*)"2Node");
 
 void AirHandler(AirPacket *pkt) {
 
@@ -10,8 +11,34 @@ void AirHandler(AirPacket *pkt) {
 int main() {
 
     air.setup();
+
+
+    while ( ! air.rf24()->isChipConnected() ) {
+        printf("Chip is not connected. Checking again in 5 seconds ");
+        int i = 0;
+        while ( i++ < 5 ) {
+            sleep(1);
+            printf(".");
+        }
+        sleep(5);
+        printf("\n");
+    }
+    printf("Chip is connected.\n");
+
     air.setHandler(&AirHandler);
+    air.rf24()->setPALevel(RF24_PA_HIGH);
     air.rf24()->printDetails();
+
+    while ( 1 ) {
+        printf("Trying to send a ping...\n");
+        sleep(5);
+        if ( air.sendPing() ) {
+            printf("Ping is sent out.\n");
+        } else { 
+            printf("Ping did not send.\n");
+        }
+    }
+
     return 0;
 }
 
